fca9ab498cab555809e4c937ef10f86794524487
lucapu88 <caputoluca88@gmail.com>
2026-03-09 11:06:56 +0100

    Added correct env apikey process

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -2,15 +2,9 @@ import { RecipeService } from '../services/recipeService.js';
 import { IconService } from '../services/iconService.js';
 
 export const generateRecipe = async (req, res) => {
-    /*  So che l'APIKEY va preso da un file .env, e in questo progetto ho impostato anche questa metodologia => const apiKey = process.env.API_KEY;
-        So anche come si setta direttamente l'apikey privata su Render (che è quello che uso per deployare il progetto).
-        Tuttavia non voglio che chiunque conosce l’URL può fare richieste (anche con curl o Postman).
-        Per questo motivo ho creato un semplice input lato frontend dove si inserisce la chiave e la salva in locale. 
-        Ma lo vedo solo io poichè il progetto frontend utilizza l'url in un app per android, mentre io uso l'url sul mio iphone da browser.
-        Quindi comunque sia NESSUNO potrà mai averla perchè la uso solo sul mio smartphone.
-    */
     try {
-        const { ingredients, apiKey } = req.body;
+        const { ingredients } = req.body;
+        const apiKey = process.env.API_KEY;
 
         if (!apiKey) {
             return res.status(400).json({ error: 'Chiave API non fornita.' });
